a45ad25a26a48a3f17f3be13a6a21307b674a3bd
Ale Feltes <afeltes@sodep.com.py>
2017-11-01 22:27:44 -0300

    Ejemplo de creación de Web Service Basado en el tutorial de Spring https://spring.io/guides/gs/rest-service/ Se implementa un protocolo básico didáctico de login REST con URL Parameters.

---
+++ gs-rest-service-complete/src/main/java/hello/LoginResponse.java
@@ -0,0 +1,23 @@
+package hello;
+
+public class LoginResponse {
+	private String message;
+	private boolean success;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+}
